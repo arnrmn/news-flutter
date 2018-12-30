2ad554ed07ccbbfb9010598eec4cd022a75264df
Arnoldas Armonas <armonas.arnoldas@gmail.com>
2018-12-30 13:51:58 +0200

    Make articles_storage.dart singleton

--- lib/articles_list/articles_storage.dart
+++ lib/articles_list/articles_storage.dart
@@ -1,4 +1,11 @@
 import 'package:news_app/article.dart';
 import 'package:news_app/utils/in_memory_cache.dart';
 
-class ArticlesStorage extends InMemoryCache<List<Article>> {}
+class ArticlesStorage extends InMemoryCache<List<Article>> {
+  static final _singleton = ArticlesStorage._internal();
+
+  ArticlesStorage._internal();
+
+  factory ArticlesStorage() => _singleton;
+
+}
