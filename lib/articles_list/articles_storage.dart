fafffe1055b889e87061cb0120fdc47dd6beec4e
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-12-31 10:35:02 +0200

    Implement articles cache

--- lib/articles_list/articles_storage.dart
+++ lib/articles_list/articles_storage.dart
@@ -1,18 +1,10 @@
 import 'package:news_app/article.dart';
-import 'package:news_app/sources_screen/source.dart';
+import 'package:news_app/utils/cache/memory_cache.dart';
 
-class ArticlesStorage {
-  Map<String, List<Article>> _cache = Map();
+class ArticlesStorage extends MemoryCache<List<Article>> {
+  static final _singleton = ArticlesStorage._internal();
 
-  Future<List<Article>> getArticles(Source source) {
-    if (_cache.containsKey(source.id)) {
-      return Future.value(_cache[source.id]);
-    } else {
-      return Future.error("No data");
-    }
-  }
+  factory ArticlesStorage() => _singleton;
 
-  Future<void> saveArticles(Source source, List<Article> articles) {
-    return Future.value(_cache[source.id] = articles);
-  }
+  ArticlesStorage._internal();
 }
--- lib/articles_list/articles_usecase.dart
+++ lib/articles_list/articles_usecase.dart
@@ -1,22 +1,25 @@
 import 'package:news_app/article.dart';
 import 'package:news_app/articles_list/articles_service.dart';
 import 'package:news_app/articles_list/articles_storage.dart';
+import 'package:news_app/sources_screen/source.dart';
+import 'package:news_app/utils/cache/cache.dart';
 
 class ArticlesUseCase {
-  final ArticlesStorage _storage;
+  final Cache<List<Article>> _storage;
   final ArticlesService _service;
 
   ArticlesUseCase({storage, service})
       : _storage = storage ?? ArticlesStorage(),
         _service = service ?? ArticlesService();
 
-  Stream<List<Article>> getArticles(source) {
-    return _storage.getArticles(source)
-        .asStream()
-        .handleError((error) {
-      _service.getArticles(source, 1).asStream().forEach((articles) {
-        _storage.saveArticles(source, articles);
-      });
-    });
+  Future<List<Article>> getArticles(Source source) async {
+    final storedArticles = _storage.get(source.id);
+    return storedArticles ??
+        _service.getArticles(source, 1).then((articles) => _save(source, articles));
+  }
+
+  List<Article> _save(Source source, List<Article> articles) {
+    _storage.put(source.id, articles);
+    return articles;
   }
 }
